09f45dd3ad2426556752097907799b08963b6f14
Mario Rizk <mario.rizk@softwaregroup.com>
2022-08-29 10:11:18 +0200

    feat(server): add /rank endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const wordList = require('./TestData.json').wordList
+const testData = require('./TestData.json')
 const getRandomWords = require('./helpers').getRandomWords
 
 const app = express()
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // Routes
 app.get('/words', (req, res) => {
   // we make a copy as we shouldn't edit the main list
-  const wordListCopy = wordList.slice()
+  const wordListCopy = testData.wordList.slice()
   const chosenWords = getRandomWords(wordListCopy, 10)
 
   if (!chosenWords) {
@@ -23,6 +23,21 @@ app.get('/words', (req, res) => {
   }
 })
 
+app.post('/rank', (req, res) => {
+  if (isNaN(req.body.score) || req.body.score > 100 || req.body.score < 0) {
+    res.status(400).json({
+      "error": "Please enter a valid exam score"
+    })
+  }
+
+  const scoresListCopy = testData.scoresList.slice()
+  const scoresBelow = scoresListCopy.reduce((curr, next) => next < req.body.score ? curr + 1 : curr, 0);
+  const finalRank = (scoresBelow * 100) / scoresListCopy.length
+
+  res.status(200).json({
+    'rank': finalRank.toFixed(2)
+  })
+})
 
 // Start the server
 const PORT = 4001
