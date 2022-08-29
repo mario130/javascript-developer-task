8462c3879e3e5ef33b07f022663b1d520320226c
Mario Rizk <mario.rizk@softwaregroup.com>
2022-08-29 12:47:29 +0200

    fix: add cors to server

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const testData = require('./TestData.json')
 const helpers = require('./helpers')
+const cors=require('cors');
 
 const app = express()
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors("*"));
 
 // Routes
 app.get('/words', (req, res) => {
