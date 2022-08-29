8fd09f0e44d5bbc5363af83dd1d000c828aa4f56
Mario Rizk <mario.rizk@softwaregroup.com>
2022-08-29 13:29:18 +0200

    feat(client): fetching questions to redux

--- client/src/app/practiceSlice.tsx
+++ client/src/app/practiceSlice.tsx
@@ -1,25 +1,70 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
+import axios from "axios";
+
+export interface questionObj {
+    id: number,
+    word: string,
+    pos: string
+}
 
 export interface PracticeState {
-    currentQuestion: number,
-    score: number
+    currentQuestionIdx: number,
+    currentQuestion: questionObj | null,
+    score: number,
+    questions: questionObj[],
+    loading: string,
+    error: string
 }
 
 const initialState: PracticeState = {
-    currentQuestion: 0,
-    score: 0
+    currentQuestionIdx: 0,
+    currentQuestion: null,
+    score: 0,
+    questions: [],
+    loading: 'idle',
+    error: ""
 }
+
+export const fetchQuestions = createAsyncThunk(
+    "practice/fetchQuestions", async (_, thunkAPI) => {
+        try {
+            const response = await axios.get(`http://localhost:4001/words`);
+            return await response.data;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.message });
+        }
+    });
 
 export const practiceSlice = createSlice({
     name: 'practice',
     initialState,
     reducers: {
         nextQuestion: (state, prevAnswer: PayloadAction<number>) => {
-            state.currentQuestion = state.currentQuestion + 1
+            state.currentQuestionIdx = state.currentQuestionIdx + 1
         },
+        setQuestions: (state, questions: PayloadAction<questionObj[]>) => {
+            state.questions = questions.payload
+        }
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchQuestions.pending, (state) => {
+            state.questions = [];
+            state.loading = "loading";
+        });
+        builder.addCase(
+            fetchQuestions.fulfilled, (state, { payload }) => {
+                // @ts-ignore
+                state.questions = payload;
+                state.loading = "loaded";
+            });
+        builder.addCase(
+            fetchQuestions.rejected,(state, action) => {
+                state.loading = "error";
+                // @ts-ignore
+                state.error = action.error.error;
+            });
+    }
 })
 
 export const { nextQuestion } = practiceSlice.actions
-
 export default practiceSlice.reducer
--- client/src/pages/Practice.tsx
+++ client/src/pages/Practice.tsx
@@ -1,16 +1,24 @@
 import {useDispatch, useSelector} from 'react-redux';
-import {nextQuestion} from "../app/practiceSlice";
+import {fetchQuestions, nextQuestion} from "../app/practiceSlice";
 import {RootState} from '../app/store';
+import {useEffect} from "react";
 
 export type RatingProps = {
-    currentQuestion?: number
+    currentQuestionIdx?: number
 }
 
 export default function Rating() {
     const dispatch = useDispatch();
 
-    const currentQuestion = useSelector((state: RootState) => state.practice.currentQuestion);
+    useEffect(()=> {
+        // @ts-ignore
+        dispatch(fetchQuestions())
+    }, [])
 
+    const currentQuestionIdx = useSelector((state: RootState) => state.practice.currentQuestionIdx);
+    const currentQuestion = useSelector((state: RootState) => state.practice.questions[currentQuestionIdx]);
+
+    const posValues = ['adjective', 'adverb', 'noun',  'verb']
     function onChangeRating() {
         dispatch(nextQuestion(2));
     }
@@ -24,24 +32,22 @@ export default function Rating() {
                     alt="star icon"
                 />
             </div>
-            <h1 className='text-2xl font-semibold my-4'>Which part of speech does this word belong?</h1>
-            <p className='text-true-gray-400 text-center py-4'>Cycling</p>
+            <h1 className='text-2xl font-semibold my-4'>Which part of speech does this word belong to?</h1>
+            <p className='text-true-gray-400 text-center py-4'>{currentQuestion ? currentQuestion.word : ''}</p>
             {/* choice buttons */}
-            <div className='flex flex-wrap pt-6 justify-center'>
+            <div className='flex flex-wrap pt-6 justify-between'>
                 {
-                    Array.from({length: 4}, (_, i) => i + 1).map(el =>
+                    posValues.map(pos =>
                         <div className={''}>
                             <button
-                                key={el}
+                                key={pos}
                                 onClick={onChangeRating}
-                                value={el}
-                                className={
-                                    `${currentQuestion === el
-                                        ? 'bg-primary text-white'
-                                        : 'bg-white hover:bg-gray-500 text-black'}
+                                value={pos}
+                                className={`
+                                     hover:bg-gray-500 
                                     active:bg-primary active:text-white transition-all  p-2 `
                                 }
-                            >{`Answer ${el}`}
+                            >{`${pos}`}
                             </button>
                         </div>
                     )
